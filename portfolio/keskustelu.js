060101a8f658c87a22e90181c2dbc565a47dced7
Aku <al0832@edu.turku.fi>
2026-04-15 13:37:02 +0300

    console loggausta

--- portfolio/keskustelu.js
+++ portfolio/keskustelu.js
@@ -1,1 +1,15 @@
 console.log("Keskustelusivu avattu")
+
+function send() {
+    let birthdayField = document.getElementById("birthday").value;
+    let emailField = document.getElementById("email").value;
+    let typeField = document.getElementById("dropdown").value;
+    let usageField = document.getElementById("agree").value;
+    let bodyField = document.getElementById("comment").value;
+    console.log(birthdayField)
+    console.log(emailField)
+    console.log(typeField)
+    console.log(usageField)
+    console.log(bodyField)
+    document.getElementById("d").close()
+}
