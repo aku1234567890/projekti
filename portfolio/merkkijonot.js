f0c4174bc4addda81165897b745f300df3ecb1f8
Aku <al0832@edu.turku.fi>
2026-05-08 14:09:32 +0300

    ,,,

--- portfolio/merkkijonot.js
+++ portfolio/merkkijonot.js
@@ -1,13 +1,25 @@
 function poimi() {
-    let a = document.getElementById("gg").value;
-    let b = document.getElementById("ff").value;
-    document.getElementById("aa").innerHTML = a[b-1]
+    let a = document.getElementById("a").value;
+    let b = document.getElementById("b").value;
+    document.getElementById("c").innerHTML = a[b-1]
 }
 
 function loyda() {
-    let a = document.getElementById("v").value;
-    let b = document.getElementById("b").value;
+    let a = document.getElementById("d").value;
+    let b = document.getElementById("e").value;
     let c = a.indexOf(b)
     c++
-    document.getElementById("bb").innerHTML = c
+    document.getElementById("f").innerHTML = c
+}
+
+function leikkaa() {
+
+}
+
+function etsi() {
+
+}
+
+function poikki() {
+    
 }
