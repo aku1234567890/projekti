1d0b45bc70aa6ff8fca3da3f5082f2329a502c8d
Aku <al0832@edu.turku.fi>
2026-05-15 14:47:06 +0300

    ,,,

--- portfolio/Henkilotiedot_luokilla.js
+++ portfolio/Henkilotiedot_luokilla.js
@@ -28,3 +28,24 @@ let persons = [
         new Person("Semir Sikäläinen", 18, "Opiskelija", true), 
         new Person("Tomas Täkäläinen", 22, "Linja-autonkuljettaja", true)
     ]
+
+let rivit = document.querySelector("tbody")
+function luoRivit() {
+    rivit.innerHTML = "";
+    for (let henkilo of persons) {
+        if (!henkilo.isUnderage()) {
+            henkilo.age = henkilo.age + "🍺"
+        }
+        if (henkilo.isStudent()) {
+            henkilo.job = henkilo.job + "🎓"
+        }
+        let rivi = document.createElement("tr")
+        for (let a in henkilo) {
+            let w = document.createElement("td")
+            w.innerHTML = henkilo[a]
+            rivi.append(w)
+        }
+        rivit.append(rivi)
+    }
+}
+luoRivit();
